d9dc6c3dcc7797a817e8912045c75267a0943810
thatsnegative <officiallynegative@protonmail.com>
2022-06-20 09:28:09 -0400

    init

---
+++ Frontend/src/app/models/Stock.model.ts
@@ -0,0 +1,6 @@
+export interface Stock {
+    symbol: string;
+    lastPrice: number;
+    netChange: number;
+    netPercentChangeInDouble: number;
+}
---
+++ Frontend/src/app/services/quotes.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuotesService {
+
+  constructor(private http:HttpClient) { }
+
+  getQuote(symbol : string): Observable<any>
+  {
+    return this.http.get<any>(`https://api.tdameritrade.com/v1/marketdata/${symbol}/quotes?apikey=FI1DGLKK127OKDAUB8VHWZEOIFM8VE5M`);
+  }
+}
