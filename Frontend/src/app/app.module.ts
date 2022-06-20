8c140a236824f611a076f0174f6a27eb90919e79
thatsnegative <officiallynegative@protonmail.com>
2022-06-20 09:13:40 -0400

    Initial

---
+++ Frontend/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavigationComponent } from './navigation/navigation.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavigationComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+
+    RouterModule.forRoot([
+      {path: "", component: HomeComponent}
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
